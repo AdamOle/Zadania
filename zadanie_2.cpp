403a2790c568b05163c8e95bc1abaac61874e10b
AdamOle <148353910+AdamOle@users.noreply.github.com>
2023-10-25 16:30:46 +0200

    Add files via upload

---
+++ zadanie_2.cpp
@@ -0,0 +1,48 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int ZliczLiczbyujemne(int AA[10])
+{
+    int ile = 0;
+    int pominiete = 0;
+    int n = 0;
+    int* BB = new int[n];
+    for (int i = 0; i < 10; i++)
+    {
+
+        if (AA[i] < 0)
+        {
+            cout << endl << "liczba ujemne:";
+            ile++;
+            BB[n] = AA[i];
+
+            cout << BB[n];
+        }
+        else
+        {
+            pominiete++;
+        }
+
+
+
+
+    }
+    cout << endl << "Liczb ujemnych: " << ile;
+    cout << endl << "Pominietych: " << pominiete;
+    return BB[n];
+}
+
+int main()
+{
+    int AA[10];
+    for (int i = 0; i < 10; i++)
+    {
+
+        cout << endl << "Wprowadz liczbe :";
+        cin >> AA[i];
+    }
+    ZliczLiczbyujemne(AA);
+
+}
+
---
+++ zadanie_4.cpp
@@ -0,0 +1,29 @@
+﻿using namespace std;
+
+#include <iostream>
+
+struct Produkt
+{
+    string nazwa;
+    double cena;
+};
+int main()
+{
+    
+    const int iloscProduktow =10;
+    Produkt produkty[iloscProduktow];
+    for (int i = 0; i < iloscProduktow; i++)
+    {
+        cout << "Podaj produkt nazwa : ";
+        cin >> produkty[i].nazwa;
+        cout<< endl << "cena : ";
+        cin >> produkty[i].cena;
+
+    }
+
+    for (int i = 0; i < iloscProduktow; i++)
+    {
+
+    }
+}
+
