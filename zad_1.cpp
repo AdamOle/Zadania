265fa48acc1bcff68e6cddb343fe97297f861bfe
AdamOle <148353910+AdamOle@users.noreply.github.com>
2023-11-07 17:51:47 +0100

    Add files via upload

---
+++ zad_1.cpp
@@ -0,0 +1,21 @@
+﻿using namespace std;
+#include <iostream>
+
+int main() {
+    for (int i = 301; i <= 1200; i += 2) {
+
+        if (i % 400 == 1) {
+            cout << "\n";
+        }
+        else if (i % 100 == 1)
+        {
+            cout << " *** ";
+        }
+        else if (i > 301) {
+            cout << " ";
+        }
+        cout << i;
+    }
+
+    return 0;
+}
