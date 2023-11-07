060b61bb528b19f2e942c4134357ada1fce65904
AdamOle <148353910+AdamOle@users.noreply.github.com>
2023-11-07 18:19:50 +0100

    zadanie_4.cpp

--- zadanie_4.cpp
+++ zadanie_4.cpp
@@ -1,29 +1,42 @@
-﻿using namespace std;
-
+using namespace std;
 #include <iostream>
+#include <string>
 
-struct Produkt
-{
+
+struct Produkt {
     string nazwa;
     double cena;
 };
-int main()
-{
-    
-    const int iloscProduktow =10;
+
+int main() {
+    const int iloscProduktow = 10;
     Produkt produkty[iloscProduktow];
-    for (int i = 0; i < iloscProduktow; i++)
-    {
-        cout << "Podaj produkt nazwa : ";
+
+    for (int i = 0; i < iloscProduktow; i++) {
+        cout << "Podaj nazwę produktu " << (i + 1) << ": ";
         cin >> produkty[i].nazwa;
-        cout<< endl << "cena : ";
+        cout << "Podaj cenę produktu " << (i + 1) << ": ";
         cin >> produkty[i].cena;
-
     }
 
-    for (int i = 0; i < iloscProduktow; i++)
-    {
-
+    //  (sortowanie bąbelkowe)
+    for (int i = 0; i < iloscProduktow - 1; i++) {
+        for (int j = 0; j < iloscProduktow - 1 - i; j++) {
+            if (produkty[j].cena < produkty[j + 1].cena) {
+                
+                Produkt temp = produkty[j];
+                produkty[j] = produkty[j + 1];
+                produkty[j + 1] = temp;
+            }
+        }
     }
+
+    
+    cout <<endl<<endl<< "Produkty od najdroższych do najtańszych:\n";
+    for (int i = 0; i < iloscProduktow; i++) {
+        cout<< "Nazwa: " << produkty[i].nazwa << ", Cena: " << produkty[i].cena << endl;
+    }
+
+    return 0;
 }
 
